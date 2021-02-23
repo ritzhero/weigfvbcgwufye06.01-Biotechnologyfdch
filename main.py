7f2b99fc33d89692370b79bd25d77e01280a8e9e
Stank Is? <76638181+ritzhero@users.noreply.github.com>
2021-02-23 13:47:53 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,51 @@
+name = input('What is your name?')
+if name == name:
+  print ("That is a very nice name.")
+age = input('How old are you?')
+if age == age: 
+  print("We are the same age, that means... nvm")
+  friend = input('Accept my friend request: yes or no?') 
+  if friend == friend:
+      print("We can be more than friends *winks*")
+      horny = input('I am just really horny rn.')
+      if horny == horny:
+        print("oki")
+        pics = input('Send pic of yourself: yes or no?')
+        if pics == pics:
+            print("Ok,I'll send a pic")
+      print("Go to th3.jpeg file to get my pic")
+      life = input('What do you do on a daily basis?')
+      if life == life:
+        print("That's interesting")
+        
+        print("I like to play sports.")
+        sports = input('Do you play sports?')
+        if sports == sports:
+          print("oh ok")
+          single = input('Are you single and ready to mingle?')
+          if single == single: 
+            print("That's nice")
+      lvl = input('I feel in love with you: yes?')
+      if lvl == lvl:
+        print("I guess that makes it official")
+print("SYSTEM: 1 day later...")
+
+baby = (input('Hey, baby, hru: good, bad, meh, ok.'))
+if baby == baby:
+  print("Ok")
+  today = input('What happened today?')
+  if today == today:
+    print("Oh")
+    boy = input('I cant believe I forgot to tell you. My name is Apollo')
+if boy == boy:
+  print("lmfao, Ty")
+  hug = input('Can I give you a hug, bb?')
+if hug == hug:
+  print("*hugs* Your hugs are the best, bb")
+  play = input('We should play: wdym?')
+  if play == play:
+    print("I can bring the toys and we can play")
+      toys = input('I want to put a toy in you')
+      if toys == toys:
+        print("Come on, let's play *winks*")
+print("SYSTEM: THE END")
